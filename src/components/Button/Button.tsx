595597f47213d47dc2529ab326bd582dfc67c6ba
Joan Kenedy <joankenedi9311@gmail.com>
2024-08-16 16:40:41 -0600

    link-whats

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,13 +5,15 @@ export function Button() {
   return (
     <div className="w-full h-full relative">
       <div className="btn-float fixed right-2 bottom-2 z-10">
-        <Image
-          src="/assets/whats.png"
-          width={70}
-          height={70}
-          alt="Picture of the author"
-          className="w-14 h-14 md:w-16 md:h-16 animate-bounce"
-        />
+        <a href="https://wa.me/525540565954" target="_blank">
+          <Image
+            src="/assets/whats.png"
+            width={70}
+            height={70}
+            alt="Picture of the author"
+            className="w-14 h-14 md:w-16 md:h-16 animate-bounce"
+          />
+        </a>
       </div>
     </div>
   );
